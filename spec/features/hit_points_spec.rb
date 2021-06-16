4b0afc01a8db4100bcfe1cd4fcd8488b688dcb59
ehelsan <ehelsan@hotmail.co.uk>
2021-06-16 19:48:20 +0100

    end of day wed

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,5 +1,6 @@
 feature 'allows you to view Player 2 hit points' do
   scenario 'display player 2 HP' do
+    
     visit '/'
     fill_in :Name_one, with: "Ehelsan"
     fill_in :Name_two, with: "E."
