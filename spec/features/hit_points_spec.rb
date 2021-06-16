9bbb7dd6bedcadab7f86e3950db12e8667221ab3
ehelsan <ehelsan@hotmail.co.uk>
2021-06-16 16:49:36 +0100

    displaying player 2 HP

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,9 @@
+feature 'allows you to view Player 2 hit points' do
+  scenario 'display player 2 HP' do
+    visit '/'
+    fill_in :Name_one, with: "Ehelsan"
+    fill_in :Name_two, with: "E."
+    click_button('Submit')
+    expect(page).to have_content "Player One: Ehelsan , Player Two: E. HP: 3"
+  end
+end
