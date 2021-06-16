9dfadf3e098a48bb6835992edd298c22debbd71b
ehelsan <ehelsan@hotmail.co.uk>
2021-06-16 14:42:23 +0100

    infrastructure working

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra/reloader' if development?
 class Battle < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
+  end
+
+  get '/' do 
+    "Testing infrastructure working!"
   end
 end
